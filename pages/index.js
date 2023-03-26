c305073c703dab84d720d2e1eceebeeefe9dd740
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-03-26 19:52:25 +1300

    Update Job-ish card with new background hover effects

--- pages/index.js
+++ pages/index.js
@@ -150,7 +150,7 @@ export default function Home() {
                 <HiOutlineExternalLink className="text-lg inline-block" />
               </div>
               {/* Job-ish */}
-              <div className=" bg-accent-red flex-1 p-7 rounded-lg my-6 max-w-sm hover:shadow-lg transition ease-in-out hover:duration-200 hover:scale-105 duration-200">
+              <div className="bg-stone-100 flex-1 p-7 rounded-lg my-6 max-w-sm hover:shadow-lg transition ease-in-out hover:bg-accent-red duration-200 hover:text-contrast-white">
                 <Image
                   className="mb-3 rounded-lg"
                   src={jobish}
@@ -158,22 +158,20 @@ export default function Home() {
                   height={215}
                   alt="Screenshot of the Job-ish website."
                 />
-                <h4 className="text-contrast-white text-lg font-semibold mb-5">
-                  Job-ish
-                </h4>
-                <p className="text-contrast-white font-light mb-5">
+                <h4 className="text-lg font-semibold mb-5">Job-ish</h4>
+                <p className="font-light mb-5">
                   Writing job descriptions has never been easier than with the
                   help of AI.
                 </p>
                 <a
-                  className="text-contrast-white text-sm font-light underline decoration-1 underline-offset-4"
+                  className="text-sm font-light underline decoration-1 underline-offset-4"
                   href="https://github.com/emelyn-barrientos/ai-jobbish-descriptions.git"
                   target="_blank"
                   rel="noreferrer"
                 >
                   Go to github
                 </a>
-                <HiOutlineExternalLink className="text-contrast-white text-lg inline-block" />
+                <HiOutlineExternalLink className="text-lg inline-block" />
               </div>
               {/* Wellington on a Beach */}
               <div className="bg-accent-blue flex-1 p-7 rounded-lg my-6 max-w-sm hover:shadow-lg transition ease-in-out hover:duration-200 hover:scale-105 duration-200">
